95c1fe95df039e869bcf1d30d1f78dbaf8fe57a9
Zachary Moseti <mosetizachary001@gmail.com>
2021-10-04 23:24:40 +0000

    Fake  data generator NPM module

--- my_app/app.js
+++ my_app/app.js
@@ -1,5 +1,12 @@
 const joke = require("one-liner-joke");
 
 const myJoke = joke.getRandomJoke();
+// console.log(myJoke);
 
-console.log(myJoke);
+// fake data generator
+
+const casual = require("casual");
+
+let city = casual.city;
+
+console.log(city);
