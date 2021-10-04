a8096b2951b59db41441ecffdfa4dfcb914f8a03
Zachary Moseti <mosetizachary001@gmail.com>
2021-10-04 23:34:39 +0000

    Defining data objects in Casual npm package

--- my_app/app.js
+++ my_app/app.js
@@ -12,4 +12,17 @@ const people = [];
 for(let i = 0; i < 20; i++) {
 	people.push(casual.full_name);
 }
-console.log(people);
+// console.log(people);
+
+const output = [];
+
+for(let j = 0; j < 10; j++) {
+	output.push({
+		firstName: casual.first_name,
+		lastName: casual.last_name,
+		city: casual.city,
+		creditCardNumber: casual.card_number(),
+		creditCardExpiry: casual.card_exp,
+	});
+}
+console.log(output);
